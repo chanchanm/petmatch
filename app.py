6b1f32596f8836558bbf87dc9d2db6b49a211d82
Channing M <markeyccm@gmail.com>
2023-12-29 13:13:55 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 def greet(name):
-    return "Hello " + name + "!!"
+    return "Hello world " + name + "!!"
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
 iface.launch()
